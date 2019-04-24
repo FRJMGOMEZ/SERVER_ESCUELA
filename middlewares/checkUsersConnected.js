b2ea468a3632e1c3176bf32237b06d43f98051dc
frjma <frjmartinezgomez@gmail.com>
2019-04-24 12:18:27 +0200

    new commit

--- middlewares/checkUsersConnected.js
+++ middlewares/checkUsersConnected.js
@@ -20,8 +20,7 @@ const checkUsersOn = (req, res, next) => {
                 })
             }
 
-            console.log(usersConnected, String(userDb._id))
-            console.log(usersConnected.includes(String(userDb._id)))
+            console.log(usersConnected && process.env.DEMO)
             if (usersConnected.includes(userDb._id) && process.env.DEMO) {
                 let message = `El usuario ${userDb.name} modo DEMO está siendo usado, prueba a loggearte con otro usuario, gracias.`
                 return res.status(200).json({ message })

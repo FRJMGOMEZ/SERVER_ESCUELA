e7de96bbd7bf44d95257ec8c98d805feb2e8804e
frjma <frjmartinezgomez@gmail.com>
2019-09-16 14:29:05 +0200

    commit to deploy

--- routes/incomes.js
+++ routes/incomes.js
@@ -46,7 +46,7 @@ app.get('/incomesNotLiquidated',[verifyToken,verifyRole],(req,res)=>{
                     if (err) {
                        return res.status(500).json({ ok: false, err })
                     }
-                    console.log(incomesDb);
+                    console.log(incomesDb.payments);
                     res.status(200).json({ ok: true, incomes: incomesDb, count })
             })
         })

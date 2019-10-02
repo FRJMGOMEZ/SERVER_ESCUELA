b1fecafacefcea5b60945df6c7326e5319ab73c2
frjma <frjmartinezgomez@gmail.com>
2019-10-02 21:19:01 +0200

    commit to deploy

--- config/awsCredentials.js
+++
@@ -1,5 +0,0 @@
-////// AWS CREDENTIALS /////
-
-process.env.AWS_ACCESS_KEY_ID = 'AKIAQ2AXEBQ4NW2DTIHA';
-process.env.AWS_SECRET_ACCESS_KEY = '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';
-process.env.S3_BUCKET_NAME = 'cargomusicfilesstorage';

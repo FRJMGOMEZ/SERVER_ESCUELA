18af6d86863e2ab031e153bc73c835b16c14878c
unknown <frjmartinezgomez@gmail.com>
2021-01-08 18:30:12 +0100

    front origin added

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(compression())
 
 /* app.use(cors()) */
  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+     res.header("Access-Control-Allow-Origin", ["*", "https://frjmgomez.github.io/FRONTEND_ESCUELA"]);
     res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Credentials, token ,query,Access-Control-Allow-Request-Method");
     res.header('Access-Control-Allow-Methods', "POST, GET, PUT, DELETE, OPTIONS");
